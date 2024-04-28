9ab9cbea8d1f15db355331c0507d79ac43822a67
cesarmr-github <c3s1r@proton.me>
2024-04-28 16:35:24 +0200

    1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+description 'ESX Identity'
+lua54 'yes'
+version '1.10.5'
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/js/script.js',
+	'html/css/style.css',
+}
+
